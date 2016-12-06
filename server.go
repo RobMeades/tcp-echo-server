fa410fe0f71e5595414202b8c01b5170ead6046d
Rob Meades <rob.meades@u-blox.com>
2016-12-06 13:37:00 +0000

    Tidy-ups to logging.

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-/* Entry point for echo server.
+/* Entry point for TCP echo server.
  *
  * This code based:
  * https://gist.github.com/paulsmith/775764
@@ -12,6 +12,7 @@ import (
     "fmt"
     "os"
     "flag"
+    "log"
 )
 
 //--------------------------------------------------------------------
@@ -23,13 +24,14 @@ import (
 //--------------------------------------------------------------------
 
 // File handle
-var pFile *os.File
+var pWriter *bufio.Writer
+var numConnections int
+var numPackets int
 
 // Command-line flags
 var pPort = flag.String ("p", "", "the port number to listen on.");
-var pFileName = flag.String ("f", "", "the file name to write the receive requests to.");
 var Usage = func() {
-    fmt.Fprintf(os.Stderr, "\n%s: run the echo server.  Usage:\n", os.Args[0])
+    fmt.Fprintf(os.Stderr, "\n%s: run the TCP echo server.  Usage:\n", os.Args[0])
         flag.PrintDefaults()
     }
 
@@ -40,17 +42,17 @@ var Usage = func() {
 // Handle a connection
 func clientConnection(listener net.Listener) chan net.Conn {
     channel := make(chan net.Conn)
-    i := 0
     
     go func() {
         for {
             client, err := listener.Accept()
             if (client != nil) {
-                i++
-                fmt.Printf("%d: %v <-> %v.\n", i, client.LocalAddr(), client.RemoteAddr())
+                numPackets = 0;
+                numConnections++
+                log.Printf("Connection %d: %v <-> %v.\n", numConnections, client.LocalAddr(), client.RemoteAddr())
                 channel <- client
             } else {
-                fmt.Printf("Couldn't accept connection (%s).\n", err.Error())
+                log.Printf("Couldn't accept connection (%s).\n", err.Error())
             }
         }
     }()
@@ -59,60 +61,46 @@ func clientConnection(listener net.Listener) chan net.Conn {
 }
 
 func handleConnection(client net.Conn) {
-    var err error 
-    var line []byte
-    
+    var err error
+    line := make([]byte, 1024)
+
     b := bufio.NewReader(client)
     for err == nil { // err will be set on EOF when the client closes
         line, err = b.ReadBytes('\n')
         if err == nil {
+            numPackets++;
             num, err1:= client.Write(line)
+            log.Printf("%d.%d: %v <-> %v: %s", numConnections, numPackets, client.LocalAddr(), client.RemoteAddr(), line)
             if err1 != nil {
-                fmt.Printf("Error, only %d of %d byte(s) could be echoed (%s).\n", num, len(line), err1.Error())    
-            }
-            if pFile != nil {
-                pFile.Write(line)
+                log.Printf("Error, only %d of %d byte(s) could be echoed (%s).\n", num, len(line), err1.Error())    
             }
         }
     }
-    fmt.Printf("Connection dropped (%s).\n", err.Error())    
+    log.Printf("Connection dropped (%s).\n", err.Error())    
 }
 
 // Entry point
 func main() {
-    var err error
-
     // Deal with the command-line parameters
     flag.Parse()
     
+    // Set up logging
+    log.SetFlags(log.LstdFlags)
+    
     if *pPort != "" {
-        // Open the output file for append
-        if *pFileName != "" {
-            pFile, err = os.OpenFile(*pFileName, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0666)
-        }    
+        // Say what we're doing
+        fmt.Printf("Echoing TCP packets received on port %s.\n", *pPort)
         
-        if err == nil {        
-            // Set up the echo server
-            fmt.Printf("Echoing packets received on port %s", *pPort)
-            if (pFile != nil) {
-                fmt.Printf(" and writing received packets to \"%s\"", pFile.Name())
+        // Set up the server
+        pServer, err := net.Listen("tcp", ":" + *pPort)
+        if pServer != nil {
+            connection := clientConnection(pServer)
+            for {
+                go handleConnection(<-connection)
             }
-            fmt.Printf(".\n")
-            
-            pServer, err := net.Listen("tcp", ":" + *pPort)
-            if pServer != nil {
-                connection := clientConnection(pServer)
-                for {
-                    go handleConnection(<-connection)
-                }
-            } else {
-                fmt.Printf("Couldn't start listening server on port %s (%s).\n", *pPort, err.Error())
-            }
-            
         } else {
-            fmt.Printf("Couldn't open file %s (%s).\n", *pFileName, err.Error())
-            os.Exit(-1);
-        }
+            fmt.Printf("Couldn't start TCP listening server on port %s (%s).\n", *pPort, err.Error())
+        }            
     } else {
         fmt.Printf("Must specify a port number.\n")
         flag.PrintDefaults()
